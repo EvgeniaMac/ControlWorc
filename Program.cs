489fcb905903016a7f551e53ceebcf681a7aa34e
«Evgenia <evgenia.kopitext@gmail.com>
2022-08-10 19:30:19 +1100

    Scheme

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿string [] array = new string [5];
+Console.WriteLine("Введите элементы массива: ");
+for (int i = 0; i< array.Length; i++)
+{
+    array[i]= Console.ReadLine();
+}
+Console.Write("Задали массив: [' " +string.Join("', ' ", array) + "']" +"-> [ ");
+
+for (int i = 0; i < array.Length; i++)
+{
+    int lenght = 3;
+    if (array[i].Length <lenght) Console.Write( array[i] +" ");
+    if (array[i].Length == lenght) Console.Write( array[i] +" ");
+}
+Console.Write( "]");
